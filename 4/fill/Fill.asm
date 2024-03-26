21eb091997e67173a3c16a74800026da223c6f40
Austin Mount <austin@adrenlinerush.net>
2024-03-26 14:53:03 -0400

    chapter 4 complete

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,76 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+  //Boolean to whether I'm fillinig or clearing screen
+
+  @bf
+  M=0
+(RESET)
+  @8192
+  D=A
+  @cnt
+  M=D
+  @SCREEN
+  D=A
+  @pixels
+  M=D
+
+
+(LOOP)
+
+  //Check Keyboard, if keystroke jump to fill
+  @KBD
+  D=M
+  @FILL
+  D;JNE
+
+  //Was I black and need to reset
+  @bf
+  D=M
+  @bf
+  M=0
+  @RESET
+  D;JGT
+
+  //whitetime
+  @pixels
+  D=M
+  A=D
+  M=0
+
+  @INC
+  0;JMP
+ 
+(FILL)
+  //Was I white and need to reset
+  @bf
+  D=M
+  @bf
+  M=1
+  @RESET
+  D;JEQ
+
+  //black time
+  @pixels
+  D=M
+  A=D
+  M=-1
+
+
+(INC)
+  @cnt
+  D=M
+  @LOOP
+  D;JEQ
+  @cnt
+  M=M-1
+  @pixels
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+   
+
+
+
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+  @cnt
+  M=0
+  @R2
+  M=0
+(LOOP)
+  //Check if I'm Done
+  @cnt
+  D=M 
+  @R1
+  D=M-D
+  @END
+  D;JEQ 
+
+  //Perform Additon of Multiplication
+  @R0
+  D=M
+  @R2
+  M=M+D
+  
+  //Increase my counter
+  @cnt
+  M=M+1
+
+  //Go Back to the beginning
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
